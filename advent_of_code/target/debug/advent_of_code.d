42eaabd16e858c394d873bf033bec80f9d514622
Mateus Pinho <mateusleitepinho@gmail.com>
2021-12-02 11:50:39 +0000

    Started rust project

---
+++ advent_of_code/target/debug/advent_of_code.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/mateu/Documents/Dev/advent-of-code/advent_of_code/target/debug/advent_of_code: /mnt/c/Users/mateu/Documents/Dev/advent-of-code/advent_of_code/src/main.rs
---
+++ advent_of_code/target/debug/deps/advent_of_code-bc133b7b5cc95cbe.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/mateu/Documents/Dev/advent-of-code/advent_of_code/target/debug/deps/advent_of_code-bc133b7b5cc95cbe: src/main.rs
+
+/mnt/c/Users/mateu/Documents/Dev/advent-of-code/advent_of_code/target/debug/deps/advent_of_code-bc133b7b5cc95cbe.d: src/main.rs
+
+src/main.rs:
